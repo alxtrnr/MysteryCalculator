f9551bea747ac190bd1beed14b3faeda846d62ea
Alex Turner <none>
2022-12-07 20:36:55 +0000

    noddling about with title output line

--- pages/answer.py
+++ pages/answer.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import time
 
-st.header(f"The number you thought of is... {st.session_state['calculation']}!")
+st.header(f"The number you thought of is...")
+x = len("The number you thought of is...")
+time.sleep(2)
+st.title(st.session_state.calculation)
 st.session_state.clear()
